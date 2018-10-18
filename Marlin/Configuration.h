12fa3f6d18b09ee21e33acc85daedeba8fc15787
kingsven <kingsven@users.noreply.github.com>
2018-10-18 20:12:14 +0800

    Update Configuration.h
    Adjusted X_MIN_POS and Z_MAX_POS to better match the dimensions of the printer

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -883,12 +883,12 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS -35
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 230
+#define Z_MAX_POS 195
 
 /**
  * Software Endstops
